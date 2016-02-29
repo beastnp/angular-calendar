b5c336b3e84892f99e9514fa21e377cd77b79a97
Nick Peterson <peterson.nick2@gmail.com>
2016-02-28 22:48:27 -0700

    calendar route working

--- core/public/app.js
+++ core/public/app.js
@@ -1,15 +1,15 @@
 var app = angular.module('calApp', ['ui.router']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
-    
+
     $stateProvider
         .state('home', {
             url: '/',
             templateUrl: 'features/calendar/calendarTmpl.html',
             controller: 'calendarCtrl'
         });
-    
+
     $urlRouterProvider
         .otherwise('/');
-    
+
 });
